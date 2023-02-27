abbfb17598dd76b7e2f0fe8eaaca1a743220415e
muhammadanasbaig <muhammmadanasasif3@gmail.com>
2023-02-27 07:04:05 +0500

    13 - 15

---
+++ app.js
@@ -0,0 +1,82 @@
+///         Q)1
+
+//             part 1
+
+// let studentNames = [ ];
+
+///            part 2
+
+// let emptyArray = {
+//    studentName : [ ]
+// }
+
+////  part 3,4,5,6
+
+// let array = ["Anas",23,false,student];
+
+///// part 7
+
+// let eduction = [
+//     "HSC",
+//     "BSC",
+//     "SSC",
+//     "BCS",
+//     "M.COm",
+//     "M.Phil",
+//     "Phd",
+//     "MS"];
+
+// document.write(eduction);
+
+//   Q)2
+
+
+// let studentNames3 = ["Michael","Jhon","Tony"]
+// let number = [320,230,480]
+
+// let studentPercentages = [ ]
+//  for (let i=0; i<number.length; i++) {
+//     let percentage = (number[i] /500) *100;
+//     studentPercentages.push(percentage);    
+//  }
+
+// for(let i=0; i<studentNames3.length;i++){
+//     console.log(studentNames3[i] + ": Score = " + number[i] + ", Percentage = " + studentPercentages[i] + "%");
+// }
+
+
+//// Q)4
+
+let studentScore = [320,230,480,120]
+console.log("Un sorted arry :" + studentScore)
+studentScore.sort();
+console.log("Sorted array in ascending order :" + studentScore);
+
+/// Q)5
+
+let cityNames = ["Karach","Lahore","Islamabad","Quetta","Peshawar"]
+console.log("Cities list:" + cityNames)
+let selected = cityNames.slice(2,-1)
+console.log("selected cities: " + selected)
+
+////   Q)6
+
+var arr = ["This","is","my","cat"];
+console.log("Array: " + arr);
+var string = arr.join('');
+console.log("String: " + string)
+
+///// Q)7
+
+let phoneManufacture = ["Apple","Samsung","Motorolla","nokia","Sony","Haier"]
+
+
+
+
+
+
+
+
+
+
+
